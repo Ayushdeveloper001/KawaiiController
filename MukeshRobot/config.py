106b0b424e7df5832cc9bfea2089b73f5cec18cb
Ayush Pandey <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-12-10 10:30:16 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -4,8 +4,8 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "" # integer value, dont use ""
-    API_HASH = ""
+    API_ID = 28397760 # integer value, dont use ""
+    API_HASH = "c5850a7b1efe26f5835a1c219e65ed54"
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
